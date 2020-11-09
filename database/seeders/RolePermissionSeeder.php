4fd46ac63fbf10f1f2aa3e718d1b0aecb1208d44
Ariful Islam Arif <arif14arif15@gmail.com>
2020-11-09 10:50:25 +0600

    roles & permission seeder done

--- database/seeders/RolePermissionSeeder.php
+++ database/seeders/RolePermissionSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 
 class RolePermissionSeeder extends Seeder
 {
@@ -13,6 +15,50 @@ class RolePermissionSeeder extends Seeder
      */
     public function run()
     {
-        //
+        //  Create roles
+        $roleSuperAdmin = Role::create(['name' => 'superadmin']);
+        $roleAdmin = Role::create(['name' => 'admin']);
+        $roleEditor = Role::create(['name' => 'editor']);
+        $roleUser = Role::create(['name' => 'user']);
+
+        //  permission List as array
+        $permissions = [
+            // Dashboard
+            'dashboard.view',
+
+            // Blog permission
+            'blog.create',
+            'blog.view',
+            'blog.edit',
+            'blog.delete',
+            'blog.approve',
+
+             // Admin permission
+             'admin.create',
+             'admin.view',
+             'admin.edit',
+             'admin.delete',
+             'admin.approve',
+
+              // Role permission
+              'role.create',
+              'role.view',
+              'role.edit',
+              'role.delete',
+              'role.approve',
+
+              // Profile permission
+              'profile.view',
+              'profile.edit',
+        ];
+
+        // Assaign Permission
+        for ($i=0; $i < count($permissions); $i++) {
+            // Create Permission
+            $permission = Permission::create(['name' => $permissions[$i]]);
+            $roleSuperAdmin->givePermissionTo($permission);
+            $permission->assignRole($roleSuperAdmin);
+        }
+
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
 
 class UserSeeder extends Seeder
@@ -14,11 +16,15 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        $user = new User();
-        $user->name = "arif";
-        $user->email = "arif@gmail.com";
-        $user->password = bcrypt('12345678');
-        $user->save();
-
+        $user = User::where('email','arif@gmail.com')->first();
+        if (is_null($user)) {
+            $user = new User();
+            $user->name = "arif";
+            $user->email = "arif@gmail.com";
+            $user->password = bcrypt('12345678');
+            $user->email_verified_at = Carbon::now();
+            $user->remember_token = Str::random(10);
+            $user->save();
+        }
     }
 }
